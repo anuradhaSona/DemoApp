c2bfffdc16999db92488f6a4ae82ee1c1f51a318
Anuradha Krishna <anuradha.krishnanov7@gmail.com>
2020-04-12 09:15:35 +0530

    add git demoApp project to local repository

---
+++ src/main/java/com/example/demo/git/App.java
@@ -0,0 +1,12 @@
+package com.example.demo.git;
+
+public class App {
+	
+	public void printData() {
+		System.out.println("printing for git!!!");
+	}
+	public static void main(String args[]) {
+		
+		System.out.println("Hello World!!!");
+	}
+}
